54da61b4fcfd9aae5d246dbf0a59b289f9863769
Michael Balint <michael.balint@pif.gov>
2016-05-13 17:43:52 -0400

    Simplified streams.

--- importer/import_trials_from_pg.js
+++ importer/import_trials_from_pg.js
@@ -3,35 +3,14 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 const fs             = require("fs");
-const stream         = require("stream");
 const pg             = require("pg");
 const QueryStream    = require("pg-query-stream");
 const JSONStream     = require("JSONStream");
 const es             = require("event-stream");
+const Transform      = require("stream").Transform;
 const Logger         = require("./logger");
 
 let logger = new Logger();
-
-// a transform stream to strip the "trial_json_object" outer json
-// container from the results
-class StripTrialContainer extends stream.Transform {
-  _transform(data, enc, next) {
-    data = data.toString();
-    let trialContainer = '{"trial_json_object":';
-    if (data.includes(trialContainer)) {
-      data = data.replace(trialContainer, "");
-      data = data.substring(0, data.lastIndexOf("}"));
-      // logger.info(`Transformed: ${data}`);
-    }
-    if (data.length >= 32) {
-      logger.info(`Piping "${data.substring(0, 32)}..." to stream...`);
-    } else {
-      logger.info(`Piping "${data}" to stream...`);
-    }
-    this.push(data);
-    return next();
-  }
-}
 
 // connect to pg
 const CONN_STRING = "postgres://localhost:5432/michaelbalint";
@@ -43,19 +22,16 @@ client.connect((err) => {
   };
   // load the query sql
   fs.readFile("trial_query.sql", "utf-8", (err, queryString) => {
+    logger.info("Querying postgres for trials...");
     // set up the streams
     let qs = client.query(new QueryStream(queryString));
     let js = JSONStream.stringify();
-    let ts = new StripTrialContainer();
     let ws = fs.createWriteStream('trials.json');
-
-    // concluding event
-    const finished = () => {
-      logger.info("Wrote contents to trials.json.");
-      client.end();
-    };
-
     // run the query and write the results to file via piping the streams
-    qs.pipe(js).pipe(ts).pipe(ws).on("finish", finished);
+    qs.pipe(js).pipe(ws).on("finish", () => {
+      logger.info("Wrote trials from 'trial_query.sql' to  'trials.json'.");
+      // disconnect from pg
+      client.end();
+    });
   });
 });
--- search/indexer/indexer.js
+++ search/indexer/indexer.js
@@ -1,13 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const JSONStream = require("JSONStream");
+
 const ElasticSearch = require("elasticsearch"),
       Logger = require("./logger");
 
+// const EsTrialFormatter = require("es_trial_formatter");
+
 const ES_HOST = "localhost",
       ES_PORT = "9200",
-      ES_INDEX = "cancer_clinical_trials";
+      ES_INDEX = "cancer-clinical-trials",
+      ES_TYPE = "trial";
 
 let logger = new Logger();
 
-module.exports = class Indexer {
+class Indexer {
 
   constructor(config) {
     this.client = new ElasticSearch.Client({
@@ -36,15 +43,43 @@ module.exports = class Indexer {
     });
   }
 
-  initMapping() {
-    logger.info(`Creating mapping for index [${ES_INDEX}].`);
+  indexDocument(doc) {
+    return this.client.index(doc);
+  }
+
+  indexFromTrialsJsonDump() {
+    let rs = fs.createReadStream(
+      path.join(__dirname, '../../importer/trials.json'));
+    let js = JSONStream.parse("*.trial_json_object");
+    logger.info("testing");
+
+    const _indexTrial = (trial) => {
+      logger.info(`Indexing clinical trial with nci_id (${trial.nci_id}).`);
+      // trial = EsTrialFormatter.format(trial);
+      this.indexDocument({
+        "index": ES_INDEX,
+        "type": ES_TYPE,
+        "id": trial.nci_id,
+        "body": trial
+      });
+    };
+
+    rs.pipe(js).on("data", _indexTrial).on("finished", () => {
+      logger.info("Indexed all trials in \"trials.json\".");
+    });
+  }
+
+  updateMapping() {
+    logger.info(`Updating mapping for index (${ES_INDEX}).`);
     return this.client.indices.putMapping({
       index: ES_INDEX,
-      type: "document",
+      type: ES_TYPE,
       body: {
         properties: require("./mapping.json")
       }
     });
   }
 
-};
+}
+
+module.exports = Indexer;
