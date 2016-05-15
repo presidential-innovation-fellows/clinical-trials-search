826f1b52f796b3ba6d26dd92199722edc1696ecd
Michael Balint <michael.balint@pif.gov>
2016-05-15 11:32:04 -0400

    Added terms searcher endpoint and logic.

--- search/api/routes/index.js
+++ search/api/routes/index.js
@@ -1,14 +1,29 @@
-var express = require('express');
-var elastic = require('elasticsearch');
-var router = express.Router();
+const express = require("express");
+const searcher = require("../searcher");
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/search', (req, res, next) => {
+router.get('/search/terms', (req, res, next) => {
+  let queryTerm = req.query.term;
+  if(!queryTerm) { respondInvalidQuery(res); }
+  let queryClassification = req.query.classification;
 
+  let q = {
+    term: queryTerm,
+    classification: queryClassification
+  }
+  searcher.searchTerms(q, (err, terms) => {
+    // TODO: add error handling
+    res.json(terms);
+  });
 });
+
+const respondInvalidQuery = (res) => {
+  return res.status(400).send("Invalid query.");
+}
 
 module.exports = router;
--- search/api/searcher.js
+++ search/api/searcher.js
@@ -1,1 +1,68 @@
-const elasticsearch = require('elasticsearch');
+const _                   = require("lodash");
+const ElasticSearch       = require("elasticsearch");
+
+const Logger              = require("../../logger");
+const CONFIG              = require("../config.json");
+
+let logger = new Logger();
+
+class Searcher {
+
+  constructor() {
+    this.client = new ElasticSearch.Client({
+      host: `${CONFIG.ES_HOST}:${CONFIG.ES_PORT}`,
+      log: Logger
+    });
+  }
+
+  _searchTermsQuery(q) {
+    return {
+      "query": {
+        "function_score": {
+          "query": {
+            "match": {
+              "text": q.term
+            }
+          },
+          "functions": [{
+            "field_value_factor": {
+              "field": "count_normalized",
+              "factor": 2.5
+            }
+          }]
+        }
+      }
+    };
+  }
+
+  searchTerms(q, callback) {
+    this.client.search({
+      index: 'cancer-terms',
+      type: 'term',
+      body: this._searchTermsQuery(q)
+    }, (err, res) => {
+      if(err) {
+        logger.error(err);
+        return callback(err);
+      }
+      return callback(null, res);
+      let formattedRes = {
+        total: res.hits.total,
+        terms: _.map(res.hits.hits, (hit) => {
+          return _.pick(hit._source, [
+            "text", "classification", "count", "count_normalized"
+          ]);
+        })
+      }
+      return callback(null, formattedRes);
+    });
+  }
+
+  searchTrials(q, callback) {
+    return callback(null, "response text");
+  }
+
+}
+
+let searcher = new Searcher();
+module.exports = searcher;
