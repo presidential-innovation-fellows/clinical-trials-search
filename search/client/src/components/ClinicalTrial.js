ddd3c040c192c128ae7bb9273e740d8f91ebf123
Michael Balint <michael.balint@pif.gov>
2016-05-16 10:06:56 -0400

    Updated clinical trial page.

--- search/client/src/components/ClinicalTrial.js
+++ search/client/src/components/ClinicalTrial.js
@@ -2,7 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import fetch from 'isomorphic-fetch';
 
-let trial = "";
+require('styles//ClinicalTrial.sass');
+
+function createMarkup(text) {
+  window.tester = text;
+  return {__html: text.replace(/(?:\r\n|\r|\n)/g, '<br />')};
+}
 
 class ClinicalTrial extends Component {
 
@@ -28,14 +33,36 @@ class ClinicalTrial extends Component {
   render() {
     let { id } = this.props.params;
     return (
-      <div>
-        <div>{id}</div>
-        <ul>
-          {this.state.trials.map((post, i) =>
-            <li key={i}>{post.nct_id}</li>
-          )}
-        </ul>
+      <div className="clinical-trial-page">
         <Link to="/">home</Link>
+        {this.state.trials.map((trial, i) =>
+          <div className="clinical-trial-detail">
+            <h1>{trial.brief_title}</h1>
+            <div>{trial.brief_summary}</div><br/>
+            <div className="clinical-trial-section">
+              <b>Sponsor:</b><br/><br/>
+              <div>{trial.sponsor.sponsor}</div>
+            </div><br/>
+            <div className="clinical-trial-section">
+              <b>Locations:</b><br/><br/>
+              <div className="clinical-trial-locations">
+                {trial.sites.map((site, i) =>
+                  <div className="clinical-trial-location">
+                    <a href={`http://maps.google.com/?q=${[site.org.name, site.org.address_line_1, site.org.city, site.org.state_or_province, site.org.postal_code].join(' ')}`}>
+                      {site.org.name}<br/>
+                      {site.org.address_line_1}<br/>
+                      {site.org.city}, {site.org.state_or_province} {site.org.postal_code}<br/>
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div><br/>
+            <div className="clinical-trial-section">
+              <b>Detailed Info:</b><br/><br/>
+              <div dangerouslySetInnerHTML={createMarkup(trial.detail_description)} />
+            </div><br/><br/><br/>
+          </div>
+        )}
       </div>
     );
   }
