d1923385c431cc5a34aba5125aaa4f6a006e3845
Michael Balint <michael.balint@pif.gov>
2016-06-06 20:09:35 -0400

    Added index for nci_thesaurus code.

--- import/import_into_pg/nci_thesaurus/import_nci_thesaurus_into_pg.sh
+++ import/import_into_pg/nci_thesaurus/import_nci_thesaurus_into_pg.sh
@@ -1,3 +1,7 @@
 # don't forget to add header to nci_thesaurus.csv...
 # code  concept_name  parents synonyms  definition
+
+# insert the table
 csvsql --insert nci_thesaurus.csv -t --db postgresql://localhost:5432/ctrp-data-warehouse
+# create the index
+psql -d ctrp-data-warehouse -c 'CREATE INDEX nci_thesaurus_code ON "public"."nci_thesaurus"(code)'
