f50591140914c44ad59f86aa65e1798720bd09d9
Michael Balint <michael.balint@pif.gov>
2016-06-07 20:17:06 -0400

    Created util methods to get query params.

--- utils/utils.js
+++ utils/utils.js
@@ -7,4 +7,45 @@ module.exports = class Utils {
       .toLowerCase();
   }
 
+  static getFlattenedMappingProperties(mapping) {
+    let props = {};
+
+    const _recurseMappingTree = (mappingTree, pathArr) => {
+      if (mappingTree["properties"]) {
+        _recurseMappingTree(mappingTree["properties"], pathArr);
+      } else if (mappingTree["type"]) {
+        props[pathArr.join(".")] = mappingTree["type"];
+      } else {
+        Object.keys(mappingTree).forEach((key) => {
+          _recurseMappingTree(mappingTree[key], pathArr.concat(key));
+        });
+      }
+    };
+
+    _recurseMappingTree(mapping, []);
+    return props;
+  }
+
+  static getFlattenedMappingPropertiesByType(mapping) {
+    let props = {};
+
+    const _recurseMappingTree = (mappingTree, pathArr) => {
+      if (mappingTree["properties"]) {
+        _recurseMappingTree(mappingTree["properties"], pathArr);
+      } else if (mappingTree["type"]) {
+        if (!props[mappingTree["type"]]) {
+          props[mappingTree["type"]] = [];
+        }
+        props[mappingTree["type"]].push(pathArr.join("."));
+      } else {
+        Object.keys(mappingTree).forEach((key) => {
+          _recurseMappingTree(mappingTree[key], pathArr.concat(key));
+        });
+      }
+    };
+
+    _recurseMappingTree(mapping, []);
+    return props;
+  }
+
 };
