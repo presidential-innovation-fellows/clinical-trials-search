f6857d0637807824899c436d0ac9cd579b5f77af
Michael Balint <michael.balint@pif.gov>
2016-06-14 22:36:52 -0400

    Updated scripts.

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,7 +15,10 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install postgres
-sudo apt-get install postgresql python-psycopg2 libpq-dev
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib python-psycopg2 libpq-dev
 
 # install csvsql
 sudo apt-get install python-pip
--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -3,3 +3,4 @@ git config --global user.name "michaelbalint"
 git config --global user.email "michael.balint@pif.gov"
 cd /opt
 git clone git@github.com:presidential-innovation-fellows/clinical-trials-search.git
+# don't forget to upload /import/import_into_pg/download_ctrp.sh and the files in /data
