9c4ed2310bb8fc48ef14f7ba6063f99e1e6aa0a3
Michael Balint <michael.balint@pif.gov>
2016-06-19 04:40:00 -0400

    Made `FilterText` component (WIP).

--- search/client/components/Search/Filter/Text/Text.js
+++ search/client/components/Search/Filter/Text/Text.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import Fetch from 'isomorphic-fetch';
 
 import Url from '../../../../lib/Url';
 
@@ -7,17 +6,62 @@ import './Text.scss';
 
 class Text extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      value: ""
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
+  }
+
   static propTypes = {
     paramField: PropTypes.string.isRequired,
     displayName: PropTypes.string.isRequired
   };
 
+  componentDidMount() {
+    let params = Url.getParams();
+    let paramValues = params[this.props.paramField] || [];
+    if (paramValues.length) {
+      this.setState({
+        value: paramValues[0]
+      });
+    }
+  }
+
+  escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    let params = {};
+    params[this.props.paramField] = [this.escapeRegexCharacters(this.state.value)];
+    Url.overwriteParams({ path: "/clinical-trials", params });
+  }
+
+  handleValueChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
-    let { displayName } = this.props;
+    let { paramField, displayName } = this.props;
+    const htmlId = paramField.split(".").join("-");
 
     return (
       <div>
-        search {displayName} by text
+        <form name={paramField} onSubmit={this.onSubmit}>
+          <label htmlFor={htmlId}>{displayName}</label>
+          <input
+            id={htmlId}
+            type="text"
+            value={this.state.value}
+            onChange={this.handleValueChange}
+          />
+        </form>
       </div>
     );
   }
