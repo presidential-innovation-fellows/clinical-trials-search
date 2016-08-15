a313fdbd592d64bc18478f20d5bd355eb2ebd71b
Bryan Pizzillo <bryanp@mail.nih.gov>
2016-08-15 16:00:10 -0400

    Added test cases for string filters. Oh and Nyan Cat reporting.

--- search/api/test/search/searcher.spec.js
+++ search/api/test/search/searcher.spec.js
@@ -1,6 +1,8 @@
 //import { describe, it } from 'mocha';
 
 import { expect } from 'chai';
+const Bodybuilder               = require("bodybuilder");
+const querystring               = require('querystring');
 const Searcher                  = require("../../search/searcher");
 const AbstractSearcherClient    = require("../../search/clients/abstract_searcher_client");
 
@@ -30,18 +32,86 @@ describe('searcher', _ => {
         let query = searcher._searchTrialById("NCT02289950");
 
         expect(query).to.eql({
-
+            query: {
+                match: {
+                    "nct_id": "NCT02289950"
+                }
+            }
         });
 
     });
 
     it('Should Build a NCI ID Query', () => {
         let searcher = new Searcher(new SearcherMockClient());
-        //let query = searcher._searchTrialById("NCI-2015-00253");
-        let query = "foo"
+        let query = searcher._searchTrialById("NCI-2015-00253");
 
         expect(query).to.eql({
+            query: {
+                match: {
+                    "nci_id": "NCI-2015-00253"
+                }
+            }
+        });
 
+    });
+
+    it('Should Add String Filter With Single Value', () => {
+        let searcher = new Searcher(new SearcherMockClient());
+        let body = new Bodybuilder();
+        let q = querystring.parse("current_trial_status=Active");
+        searcher._addStringFilters(body, q);
+
+
+        expect(body.build("v2")).to.eql({
+            query: {
+                bool: {
+                    filter: {
+                        term: {
+                            "current_trial_status": "active"
+                        }
+                    }
+                }
+            }
+        });
+
+    });
+
+    it('Should Add String Filter with multiple values', () => {
+        let searcher = new Searcher(new SearcherMockClient());
+        let body = new Bodybuilder();
+        let q = querystring.parse("current_trial_status=Active&current_trial_status=Temporarily+Closed+to+Accrual");
+        searcher._addStringFilters(body, q);
+
+
+        expect(body.build("v2")).to.eql({
+            query: {
+                bool: {
+                    filter: {
+                        bool: {
+                            "must": [
+                                {
+                                "query": {
+                                    "bool": {
+                                        "filter": {
+                                            "bool": {
+                                                "should": [
+                                                    {
+                                                        "term": { "current_trial_status": "active" }
+                                                    },
+                                                    {
+                                                        "term": { "current_trial_status": "temporarily closed to accrual" }
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    }
+                                }
+                                }
+                            ]                                                        
+                        }
+                    }
+                }
+            }
         });
 
     });
